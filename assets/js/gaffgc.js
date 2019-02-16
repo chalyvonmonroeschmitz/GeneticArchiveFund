f0fa03eb7a109585d75618653146208b4044b439
Charly <charly@outlook.com.au>
2019-02-16 21:40:00 +1030

    Updated JS assets and media files for html and js

---
+++ assets/js/gaffgc.js
@@ -0,0 +1,22 @@
+﻿$(document).load(function ($) { });
+
+
+$(document).ready(function ($) {
+
+    $.getScript("/assets/js/xenon-custom.js", function () {
+        console.log("xenon-custom script imported");
+    });
+
+    // custom validators
+    $.validator.unobtrusive.adapters.addBool("MustBeChecked", "required");
+
+    // UA checkbox 
+    $('input[name=AcceptedUA]').change(function (event) {
+        if ($(this).is(":checked")) {
+            $('#submitbutton').removeClass("disabled");
+        } else {
+            $('#submitbutton').addClass("disabled");
+        }
+    });
+
+});
